21b7d3dfe623da3869ea7e818c1c53e264a82bb2
Frederick Morlock <frederickgeek8@gmail.com>
2025-05-07 09:41:21 -0400

    Bumped version to 1.29.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.nutomic.syncthingandroid"
         minSdk = 21
         targetSdk = 33
-        versionCode = 4396
-        versionName = "1.29.2"
+        versionCode = 4397
+        versionName = "1.29.6"
         testApplicationId = "com.nutomic.syncthingandroid.test"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
